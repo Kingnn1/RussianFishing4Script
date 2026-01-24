f52b339e735788fa20301c0ebf60a21827d4f54f
Kingnn1 <46890766+Kingnn1@users.noreply.github.com>
2026-01-24 09:25:04 +0100

    Adding_Jitter
    Adding jitter for system parametrs and  config one for other

--- rf4s/config/defaults.py
+++ rf4s/config/defaults.py
@@ -103,6 +103,8 @@ _C.BOT.PAUSE_DURATION = 600
 # Whether the Windows ClickLock is enabled.
 # The time you need to hold down the mouse button must be set to "long".
 _C.BOT.CLICK_LOCK = False
+# Jitter for various time delays (0.0-1.0)
+_C.BOT.JITTER = 0.2
 
 
 # ---------------------------------------------------------------------------- #
